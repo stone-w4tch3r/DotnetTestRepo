b9fba51ce0efd59be2b612c3dc664ef7787141ca
dbizimov <d.bizimov@e-promo.ru>
2023-01-29 20:43:12 +0500

    14.cs

--- Test/14.cs
+++ Test/14.cs
@@ -1,1 +1,10 @@
 Console.WriteLine("Hello World");
+
+IEnumerable<int> ids = new[] { 10, 20 };
+var ids2 = ids.Append(1);
+
+Console.WriteLine(ids.Count());
+Console.WriteLine(ids2.Count());
+Console.WriteLine(ReferenceEquals(ids, ids2));
+Console.WriteLine(ids.GetType());
+Console.WriteLine(ids2.GetType());
