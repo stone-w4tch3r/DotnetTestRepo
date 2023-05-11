98969846429274e40143834e841381de0829962b
stone-w4thc3r <no@e.mail>
2023-05-11 11:59:58 +0500

    composite disposables experiments

---
+++ Test/25.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Reactive.Disposables;
+
+Console.WriteLine();
+
+var cd = new CompositeDisposable();
+
+Console.WriteLine(cd.Count);
+
+var cdAsCollection = (ICollection<IDisposable>)cd;
+
+var d1 = Disposable.Create(() => Console.WriteLine("d1"));
+
+cd.Add(d1);
+
+Console.WriteLine(cd.Count);
