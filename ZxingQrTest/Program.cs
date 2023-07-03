cfa15a50426aed95b2243b51cdbb7d08f6f9ebd2
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-07-03 20:11:09 +0500

    tests commented out (unfinished)

--- ZxingQrTest/Program.cs
+++ ZxingQrTest/Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+using ZXing.Rendering;
+
 Console.WriteLine("Hello, World!");
+
+var options =  new QrCodeEncodingOptions
+{
+    DisableECI = true,
+    CharacterSet = "UTF-8",
+    Width = 250,
+    Height = 250,
+};
+// var writer = new BarcodeWriter();
+// writer.Format = BarcodeFormat.QR_CODE;
+// writer.Options = options; 
+// var matrix = writer.Encode(str);
+// var svgImage = ConvertToSvgImage(matrix);
+
+
+SvgRenderer.SvgImage ConvertToSvgImage(BitMatrix matrix)
+{
+    var renderer = new SvgRenderer();
+    var svgImage = renderer.Render(matrix, BarcodeFormat.QR_CODE, "hello");
+    return svgImage;
+}
