10f8ccfe1f9841da0283c142849a4baa0e3aee40
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-07-18 00:35:02 +0500

    28, bugfix

--- Test/28.cs
+++ Test/28.cs
@@ -1,2 +1,13 @@
 Console.WriteLine("hello");
 
+var myClass = new MyClass { Text = "world" };
+
+public class MyClass
+{
+    public required string Text { get; init; }
+
+    public MyClass()
+    {
+        Console.WriteLine(Text);
+    }
+}
