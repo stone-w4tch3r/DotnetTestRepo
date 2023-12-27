8f58115e236b4fcde1819fb3ebdb5a9b6e899d43
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-28 04:22:23 +0500

    added FluentResultsTest

---
+++ FluentResultsTest/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FluentResults;
+
+var action = new Action(() => throw new Exception("test"));
+var result = Result.Try(action);
+
+var worker = new Worker();
+var result2 = Result.Try(worker.DoWork);
+
+result.WithErrors(result2.Errors)
+    .WithError($"error {new List<int> {1, 2, 3}}");
+
+Console.WriteLine(result);
+
+public class Worker
+{
+    public void DoWork()
+    {
+        throw new NotImplementedException();
+    }
+}
