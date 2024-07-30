902d3593e23045ec616fc83458590064517836cc
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-07-30 12:59:13 +0500

    30 improved

--- BasicCsharpTest/30.cs
+++ BasicCsharpTest/30.cs
@@ -1,27 +1,86 @@
+// ReSharper disable InconsistentNaming
+
+using System.Diagnostics.CodeAnalysis;
+
 Console.WriteLine("start");
 
-var x = 0;
-object locker = new(); // объект-заглушка
-// запускаем пять потоков
-for (var i = 1; i < 6; i++)
+// BasicLockShowcase.Run();
+LockMonitorImplementation.Run();
+// LockMonitorImplementation.DeadlockIfNotExitDueToException();
+
+//.NET 9 has special System.Threading.Lock with non-monitor implementation
+
+internal static class BasicLockShowcase
 {
-    var myThread = new Thread(Print) { Name = $"Поток {i}" };
-    myThread.Start();
+    private static int x = 0;
+    private static readonly object locker = new(); // объект-заглушка
+
+    public static void Run()
+    {
+        for (var i = 1; i < 6; i++) // запускаем пять потоков
+        {
+            var t = new Thread(() =>
+                {
+                    x = 1;
+                    // lock (locker)
+                    {
+                        for (var i1 = 1; i1 < 6; i1++)
+                        {
+                            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+                            x++;
+                            Thread.Sleep(100);
+                        }
+                    }
+                })
+                { Name = $"Поток {i}" };
+            t.Start();
+        }
+    }
 }
 
-return;
-
-
-void Print()
+// lock (locker) - это синтаксический сахар для Monitor.Enter и Monitor.Exit
+internal static class LockMonitorImplementation
 {
-    x = 1;
-    lock (locker)
+    private static int x = 0;
+    private static readonly object locker = new(); // объект-заглушка
+
+    public static void Run()
     {
-        for (var i = 1; i < 6; i++)
+        for (var i = 1; i < 6; i++) // запускаем пять потоков
         {
-            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
-            x++;
-            Thread.Sleep(100);
+            var t = new Thread(() =>
+                {
+                    x = 1;
+                    Monitor.Enter(locker);
+                    for (var i1 = 1; i1 < 6; i1++)
+                    {
+                        Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+                        x++;
+                        Thread.Sleep(100);
+                    }
+
+                    Monitor.Exit(locker);
+                })
+                { Name = $"Поток {i}" };
+            t.Start();
         }
+    }
+    
+    public static void DeadlockIfNotExitDueToException()
+    {
+        var t = new Thread(() =>
+        {
+            Monitor.Enter(locker);
+            throw new ("Exception in thread");
+            Monitor.Exit(locker);
+        });
+        t.Start();
+        var t2 = new Thread(() =>
+        {
+            Monitor.Enter(locker);
+            Console.WriteLine("This will never be printed");
+            Monitor.Exit(locker);
+        });
+        t2.Start();
     }
 }
